d0946b378b79aefaef7f9f379dcae80936fc3877
Eric Windmill <ericwindmill@gmail.com>
2018-01-15 12:40:11 -0800

    make 4 work foreal

--- 04/finish/web/main.dart
+++ 04/finish/web/main.dart
@@ -4,9 +4,20 @@ import 'dart:async';
 void main() {
   var menuChapters = document.querySelectorAll('li > a');
   window.addEventListener("scroll", (e) => onScroll(e, menuChapters));
+
+  var chapterLinks = querySelectorAll('nav > ul li a');
+  chapterLinks.forEach((link) {
+    link.addEventListener('click', (e) {
+      var currentActive = document.querySelector(".active-chapter");
+      currentActive.classes.remove('active-chapter');
+      link.classes.add('active-chapter');
+    });
+  });
 }
 
-EventListener onScroll(Event e, List<Element> menuChapters) {
+void handleClick(CustomEvent e) {}
+
+void onScroll(CustomEvent e, List<Element> menuChapters) {
   var scrollPos = window.pageYOffset;
   menuChapters.forEach((chapter) {
     var currentActive = document.querySelector(".active-chapter");
@@ -19,34 +30,4 @@ EventListener onScroll(Event e, List<Element> menuChapters) {
       chapter.classes.add('active-chapter');
     }
   });
-}
-
-class HeadingController {
-  final List<Element> menuChapters;
-  final Element section;
-  int topYpos;
-  int bottomYpos;
-
-  HeadingController(this.section, this.menuChapters) {
-    topYpos = section.documentOffset.y;
-    bottomYpos = section.documentOffset.y + section.offsetHeight;
-    var halfSec = new Duration(milliseconds: 500);
-    new Timer.periodic(halfSec, (Timer t) => listenForScrollChange());
-  }
-
-  listenForScrollChange() {
-    if (window.pageYOffset > topYpos && window.pageYOffset < bottomYpos) {
-      print(window.pageYOffset > topYpos && window.pageYOffset < bottomYpos);
-      String navId = getCorrespondingId(section);
-      var navLi = querySelector('#$navId');
-      var currentActive = querySelector('.active-chapter');
-      print(currentActive.text);
-      currentActive.classes.remove('active-chapter');
-      navLi.classes.add('eactive-chapter');
-    }
-  }
-
-  String getCorrespondingId(Element el) {
-    return el.id.split("-")[1];
-  }
 }
